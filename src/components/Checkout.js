ddbb739af31cda42500fc2eb2c9bd2d2f2bc5707
Salvato Luice <salvatoluice5@gmai.com>
2022-11-18 17:10:32 +0300

    Firebase authentication

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,7 +6,7 @@ import Checkoutproduct from './CheckoutProduct'
 
 const Checkout = () => {
 
-  const [ { basket }, dispatch] = useStateValue();
+  const [ { basket, user }, dispatch] = useStateValue();
 
 
   return (
@@ -14,6 +14,7 @@ const Checkout = () => {
         <div className='checkput_left'>
             <img className='checkout-ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' />
             <div>
+              <h3>Hello, {user?.email}</h3>
                 <h2 className='checkout_title'>Your Shopping Cart</h2>
                 {basket.map(item => (
                   <Checkoutproduct 
